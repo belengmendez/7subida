4911d7528e4222db21298147b2c6955177efaee0
belengmendez <damcb164e19@gmail.com>
2015-05-28 10:29:32 -0500

    3subir

--- js/acciones2.js
+++ js/acciones2.js
@@ -1,45 +1,52 @@
 // JavaScript Document
-  
+
+
 $(document).ready(function(e){
-  document.addEventListener("deviceready",function(){
-	  
-	  $('#izquierda').on("swipeleft",function(){
-		  navigator.notification.alert("Deslizo a la izquierda",function(){"Aplicacion5","Aceptar"});
-	  });//barrer izquierda
-	  
-	  
-	$('#derecha').on("swipeleft",function(){ 
-	 navigator.notification.confirm("¿Que quiere hacer?",function(opt){
-		 switch(opt)
-	 {
-		 
-		 case 1:
-		 navigator.notification.beep(1);
-		 break;
-		 
-		 case 2:
-		  navigator.notification.vibrate(100);
-		 break;
-	 }//switch opt
-	 },"Aplicacion5","Beep,Vibrar,Cancelar");//confirm
-	 
-	});//barrer derecha
-	
-	
-  },false);//ready device
+   document.addEventListener("deviceready",function(){
+       $('#disp table td').eq(3).text(device.model);
+       $('#disp table td').eq(5).text(device.cordova);
+       $('#disp table td').eq(7).text(device.platform);
+       $('#disp table td').eq(9).text(device.version);
+       $('#disp table td').eq(11).text(device.uuid);
+
+       document.addEventListener("pause",function(){  
+           escribehistoria('la app se pauso');
+       },false;//pause
+
+       document.addEventListener("resume",function(){
+           escribehistoria('la app se reinicio');
+       },false;//resume
+
+
+       document.addEventListener("online",function(){
+           escribehistoria('la app se conecto a la red');
+       },false;//se conecto
+
+
+       document.addEventListener("offline",function(){
+           escribehistoria('la app se desconecto de la red');
+       },false;//se desconecto
+
+       },false;//ready device
+
 });//document
-	 
-		 
-	  
-     
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
+
+function escribehistoria(accion){
+     $('#eHistoria').append('<li>+accion+'</li>');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
